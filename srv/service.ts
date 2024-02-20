0bdbb4e888bbd343ee86d95e9227d8c91be3b982
Aryan Raj Sinha <aryan.raj.sinha@sap.com>
2024-02-20 13:09:46 +0000

    changes

--- srv/service.ts
+++ srv/service.ts
@@ -1,29 +1,56 @@
-import {
-    retrieveJwt,
-} from "@sap-cloud-sdk/connectivity";
 
+import cds, { ApplicationService } from '@sap/cds'
 import { WorkflowInstancesApi as wfApi } from "./generated/SPA_Workflow_Runtime"
 
-
-module.exports = async function (srv: any) {
-    const {
-        Header, AssetDetails, RequestPurpose, RequestType, OS, Manufacturer, AssetCategory, PickUpType, RequestNumber
-    } = srv.entities;
-
-
-    srv.before("NEW", Header.drafts, async (req: any) => {
-        req.data.assetDetails ??= {};
-    });
-
-    srv.after("CREATE", Header, async (req: any) => {
-        console.log(req)
-        wfApi.createV1WorkflowInstances({
-            definitionId: "eu10.sap-process-automation-tfe.zitatechitassetrequest.iTAssetRequestProcess",
-            context: {requestid: req.ID}
-        }).skipCsrfTokenFetching()
-        .execute({
-            destinationName: "sap-bpa-workflow-api",
+class ITAssetRequestFormService extends ApplicationService {
+    async init() {
+        console.log("Service JS Triggered")
+        this.before("NEW", "Header.drafts", async (req: any) => {
+            console.log("Header Drafts New triggered")
+            req.data.assetDetails ??= {};
         });
-    })
 
+        this.after("CREATE", "Header", async (req: any) => {
+            console.log(req)
+            wfApi.createV1WorkflowInstances({
+                definitionId: "eu10.sap-process-automation-tfe.zitatechitassetrequest.iTAssetRequestProcess",
+                context: { requestid: req.ID }
+            }).skipCsrfTokenFetching()
+                .execute({
+                    destinationName: "sap-bpa-workflow-api",
+                });
+        })
+        return super.init()
+    };
 }
+module.exports = { ITAssetRequestFormService }
+
+// import {
+//     retrieveJwt,
+// } from "@sap-cloud-sdk/connectivity";
+
+// import { WorkflowInstancesApi as wfApi } from "./generated/SPA_Workflow_Runtime"
+
+
+// module.exports = async function (srv: any) {
+//     const {
+//         Header, AssetDetails, RequestPurpose, RequestType, OS, Manufacturer, AssetCategory, PickUpType, RequestNumber
+//     } = srv.entities;
+
+
+//     srv.before("NEW", Header.drafts, async (req: any) => {
+//         req.data.assetDetails ??= {};
+//     });
+
+//     srv.after("CREATE", Header, async (req: any) => {
+//         console.log(req)
+//         wfApi.createV1WorkflowInstances({
+//             definitionId: "eu10.sap-process-automation-tfe.zitatechitassetrequest.iTAssetRequestProcess",
+//             context: {requestid: req.ID}
+//         }).skipCsrfTokenFetching()
+//         .execute({
+//             destinationName: "sap-bpa-workflow-api",
+//         });
+//     })
+
+// }
